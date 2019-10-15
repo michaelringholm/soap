949b8070277b41294170caafa772da5f059c83fd
Michael Ringholm Sundgaard <michaelringholm@gmail.com>
2019-10-15 12:11:57 +0200

    Added SOAPWithString sample

--- dotnet/soap-client/Program.cs
+++ dotnet/soap-client/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Net.Http;
 using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace soap_client
 {
@@ -7,14 +12,38 @@ namespace soap_client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            CallSOAPEndpoint();
+            Console.WriteLine("Starting SOAP client sample...");
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var builder = new ConfigurationBuilder().AddJsonFile($"appsettings.{env}.json", optional: false, reloadOnChange: true);
+            var configuration = builder.Build();
+            var soapWSDLURL = configuration.GetValue<string>("SOAPWSDLURL");
+            var serviceCollection = new ServiceCollection();
+
+            CallSOAPViaStubs(soapWSDLURL);            
+            CallSOAPEndpointWithString(soapWSDLURL).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
-        private static void CallSOAPEndpoint()
+        private static async Task CallSOAPEndpointWithString(string soapWSDLURL)
+        {
+            var httpClient = new HttpClient();
+            var xml = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ss=\"http://tempuri.org/\">" +
+            "<soapenv:Body>" +
+            "<ss:Echo>" +
+                "<ss:msg>Please echo this</ss:msg>" +
+            "</ss:Echo>" +
+            "</soapenv:Body>" +
+            "</soapenv:Envelope>";
+            var httpContent = new StringContent(xml, Encoding.UTF8, "application/xml");
+
+            var result = await httpClient.PostAsync(soapWSDLURL, httpContent);
+            var content = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(result.StatusCode);
+        }
+
+        private static void CallSOAPViaStubs(string soapWSDLURL)
         {
             var binding = new BasicHttpsBinding();
-            var endpoint = new EndpointAddress(new Uri("<url>"));
+            var endpoint = new EndpointAddress(new Uri(soapWSDLURL));
             dynamic channelFactory = new ChannelFactory<ISimpleSOAPService>(binding, endpoint);
             var serviceClient = channelFactory.CreateChannel();
             var result = serviceClient.Ping();
